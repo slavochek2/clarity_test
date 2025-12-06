4167e6fcb52c497317923e5c5a35691789689bef
Vyacheslav Ladischenski <slavochek@googlemail.com>
2025-12-06 15:38:47 +0700

    Remove 'Learn more' link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,6 @@ export default function Home() {
           >
             Get started
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
-            Learn more <span aria-hidden="true">→</span>
-          </a>
         </div>
       </div>
     </main>
